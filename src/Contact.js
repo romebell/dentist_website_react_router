189c89e25a44e2287c3281e7ca0b8833cf70e1f8
Rome Bell <romebell5@gmail.com>
2020-08-28 10:59:18 -0700

    [contact, procedures] Add Contact and Procedures components

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Contact Us</h1>
+                <p>Rome Bell, MD</p>
+                <p>Email: rome.bell@dentist.com</p>
+                <p>Phone: 111-111-1111</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
