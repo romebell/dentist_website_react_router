86aec209012143403582c4e4a0d546a54a18e591
Rome Bell <romebell5@gmail.com>
2020-08-28 11:30:14 -0700

    [procedures] Add procedures as props into component

--- src/Procedures.js
+++ src/Procedures.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 
 class Procedures extends Component {
     render() {
+        const proceduresList = this.props.procedures.map((p, idx) => {
+            console.log(idx);
+            return <li key={idx}>{p}</li>
+        });
+        // console.log(this.props.procedures)
         return (
             <div>
                 <h1>Procedures</h1>
+                {proceduresList}
             </div>
         );
     }
